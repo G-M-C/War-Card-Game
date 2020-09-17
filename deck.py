c3771609e94ba3696fab8903e06535083aeaba1e
George M Cherian <42828441+G-M-C@users.noreply.github.com>
2020-09-17 12:31:15 +0530

    Class for Deck Object

---
+++ deck.py
@@ -0,0 +1,26 @@
+import random
+from card import Card
+
+
+suits=('Hearts','Diamonds','Spades','Clubs')
+ranks=('Two','Three','Four','Five','Six','Seven','Eight','Nine','Ten','Jack','Queen','King','Ace')
+
+
+
+class Deck:
+
+    def __init__(self):
+
+        self.all_cards = []
+
+        for suit in suits:
+            for rank in ranks:
+                new_card = Card(suit, rank)
+                self.all_cards.append(new_card)
+
+    def shuffle_cards(self):
+
+        random.shuffle(self.all_cards)
+
+    def deal_one(self):
+        return self.all_cards.pop()
